cde14aeb2b31fa47514cd92c19b18eb504eb0689
Gustavo Gabriel <gustavogabrielsf@gmail.com>
2025-09-12 21:33:31 -0300

    Versão 1 do Projeto Aula

---
+++ Projeto Aula/Controllers/ContatoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projeto_Aula.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
